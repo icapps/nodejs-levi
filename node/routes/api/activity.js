cb74656aaafca3b0eb84b947bd4f3ee32942bac5
Jelle Vandebeeck <jelle@fousa.be>
2016-05-21 15:02:15 +0200

    Find and update the given activity.

--- node/routes/api/activity.js
+++ node/routes/api/activity.js
@@ -1,10 +1,29 @@
 'use strict';
 
+var keystone = require('keystone'),
+    Activity = keystone.list('Activity'),
+    moment = require('moment');
+
 module.exports = {
 
     postActivities: function(req, res) {
-        res.status(200).send({
-            message: 'okdoki'
+        Activity.model.findOneAndUpdate({
+            user:      req.body.userId,
+            timestamp: moment(req.body.timestamp).startOf('hour'),
+            type:      req.body.type
+        }, {
+            value: req.body.value
+        }, {
+            new: true,
+            upsert: true
+        }, function(err, activity) {
+            if (err) {
+                res.status(404).send();
+            } else {
+                res.status(200).send({
+                    activity: activity
+                });
+            }
         });
     }
 };
