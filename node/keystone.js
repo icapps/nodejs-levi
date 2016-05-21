87cbf79a507e700a8be00ae6cce12488fec606c2
Mo Binni <mo.binni@hotmail.com>
2016-05-21 17:35:11 +0200

    fixes

--- node/keystone.js
+++ node/keystone.js
@@ -16,4 +16,55 @@ keystone.set('nav', {
     'users': 'users'
 });
 
-keystone.start();
+keystone.start({
+    onHttpServerCreated: function () {
+        keystone.set('io', socketio.listen(keystone.httpServer));
+    },
+    onStart: function () {
+        var io = keystone.get('io');
+        var message = require('./routes/api/message');
+
+        var alchemy_language = watson.alchemy_language({
+            api_key: 'b436fe0c4a13f73bc30fb5e4943b627cf6ac6d8b'
+        });
+
+        // Socketio connection
+        io.on('connect', function (socket) {
+            console.log('--- User connected');
+
+            socket.on('chat', function (data) {
+                socket.join('room/' + data.roomID);
+            });
+
+            socket.on('message', function (data) {
+                io.to('room/' + data.roomID).emit('response', data.message);
+                // TODO: implement bluemix endpoint
+                var params = {
+                    text: data.message
+                };
+
+                alchemy_language.sentiment(params, function (err, response) {
+                    if (err) {
+                        console.log('watson error:', err);
+                    }
+                    else {
+                        console.log(response)
+                        message.save(data.roomID, data.userID, data.message, response['docSentiment']['type'] || '',
+                            response['docSentiment']['score'] || 0, function (result) {
+                            if (result.error) {
+                                console.log(result.error);
+                            }
+                            else {
+                                io.to('room/' + data.roomID).emit('sentiment', response['docSentiment']);
+                            }
+                        })
+                    }
+                });
+            })
+
+            socket.on('disconnect', function () {
+                console.log('--- User disconnected');
+            });
+        });
+    }
+});
