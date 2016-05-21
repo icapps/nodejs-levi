be496b8ffb25baccba9f9f9969ff080c74123d43
Jelle Vandebeeck <jelle@fousa.be>
2016-05-21 15:34:43 +0200

    Make it possible to upload more data.

--- node/routes/api/activity.js
+++ node/routes/api/activity.js
@@ -2,28 +2,45 @@
 
 var keystone = require('keystone'),
     Activity = keystone.list('Activity'),
-    moment = require('moment');
+    moment = require('moment'),
+    _ = require('lodash');
 
 module.exports = {
 
     postActivities: function(req, res) {
-        Activity.model.findOneAndUpdate({
-            user:      req.body.userId,
-            timestamp: moment(req.body.timestamp).startOf('hour'),
-            type:      req.body.type
-        }, {
-            value: req.body.value
-        }, {
-            new: true,
-            upsert: true
-        }, function(err, activity) {
-            if (err) {
-                res.status(404).send();
-            } else {
-                res.status(200).send({
-                    activity: activity
-                });
-            }
+        var userId = req.body.userId;
+        var activities = req.body.activities;
+
+        var promises = [];
+        _.forEach(activities, function(activity) {
+           promises.push(
+               new Promise(function(resolve, reject) {
+                   Activity.model.findOneAndUpdate({
+                       user:      userId,
+                       timestamp: moment(activity.timestamp).startOf('hour'),
+                       type:      activity.type
+                   }, {
+                       value: activity.value
+                   }, {
+                       new:    true,
+                       upsert: true
+                   }, function(err, activity) {
+                       if (err) {
+                           reject();
+                       } else {
+                           resolve(activity);
+                       }
+                   });
+               })
+           );
+        });
+
+        Promise.all(promises).then(function(activities) {
+            res.status(200).send({
+                activities: activities
+            });
+        }, function(err) {
+            res.status(400).send();
         });
     }
 };
