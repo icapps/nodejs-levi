657217adc77e5dd19d331da7af495bf19fc52f0e
Mo Binni <mo.binni@hotmail.com>
2016-05-21 22:12:20 +0200

    yolo

--- app/js/page/controller/home.controller.js
+++ app/js/page/controller/home.controller.js
@@ -17,7 +17,7 @@
 
         vm.send = function () {
             socket.emit('message', { userID: vm.userID, message: vm.message, roomID: '574076e2e0474103008091c6'})
-            vm.messages.push({ userID: vm.userID, message: vm.message });
+            //vm.messages.push({ userID: vm.userID, message: vm.message });
             vm.message = '';
         }
     }
